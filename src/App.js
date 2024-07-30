8347b3f44756589698145256af62aa9a1e4cdde0
Usman0889 <120021411+Usman0889@users.noreply.github.com>
2024-07-30 03:59:18 -0700

    todolist commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TodoWrapper from './compenents/TodoWrapper';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoWrapper />
+    </div>
+  );
+}
+
+export default App;
